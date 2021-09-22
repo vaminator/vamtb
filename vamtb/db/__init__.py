6c4ca24360935d57510517b1569422c47878f99d
vamtb <foo@bar.com>
2021-09-22 20:03:02 +0200

    Remove useless code

--- vamtb/db/__init__.py
+++ vamtb/db/__init__.py
@@ -47,34 +47,33 @@ def store_var(conn, var):
     if not rows:
         modified_time = os.path.getmtime(var)
         creator, version, modified_time,cksum=varfile.get_props(var)
-        if creator in ref_creators:
-            v_isref="YES" 
-        else:
-            v_isref="UNKNOWN"
+        v_isref="YES" if creator in ref_creators else "UNKNOWN"
+
         meta=varfile.extract_meta_var(var)
         license=meta['licenseType']
-        row = (varname, v_isref, creator, version, license, modified_time, cksum)
-        sql = """INSERT INTO VARS(VARNAME,ISREF,CREATOR,VERSION,LICENSE,MODIFICATION_TIME,CKSUM) VALUES (?,?,?,?,?,?,?)"""
+
         cur = conn.cursor()
+        sql = """INSERT INTO VARS(VARNAME,ISREF,CREATOR,VERSION,LICENSE,MODIFICATION_TIME,CKSUM) VALUES (?,?,?,?,?,?,?)"""
+        row = (varname, v_isref, creator, version, license, modified_time, cksum)
         cur.execute(sql, row)
+
         with ZipFile(var, mode='r') as myvar:
             listOfFileNames = myvar.namelist()
             for f in listOfFileNames:
-                # logging.debug(f"Computing crc of {f}")
                 with myvar.open(f) as fh:
                     try:
                         crcf = varfile.crc32c(fh.read())
                     except:
                         #FIXME zlib.error: Error -3 while decompressing data: invalid code lengths set
+                        logging.error(f'Crc computation error on file {var}.')
                         return
-                if creator in ref_creators:
-                    f_isref="YES" 
-                else:
-                    f_isref="UNKNOWN"
-                row = (None, f, f_isref, varname, crcf)
-                sql = """INSERT INTO FILES (ID,FILENAME,ISREF,VARNAME,CKSUM) VALUES (?,?,?,?,?)"""
+                f_isref="YES" if creator in ref_creators else "UNKNOWN"
+
                 cur = conn.cursor()
+                sql = """INSERT INTO FILES (ID,FILENAME,ISREF,VARNAME,CKSUM) VALUES (?,?,?,?,?)"""
+                row = (None, f, f_isref, varname, crcf)
                 cur.execute(sql, row)
+
         logging.debug(f"Stored var {varname} and files in databases")
         sql = """INSERT INTO DEPS(ID,VAR,DEP) VALUES (?,?,?)"""
         for dep in varfile.dep_fromvar(dir=None, var=var, full=True):
@@ -82,12 +81,12 @@ def store_var(conn, var):
             cur = conn.cursor()
             cur.execute(sql, row)
     else:
-        assert(len(rows) == 1)
+        assert( len(rows) == 1 )
         db_varname, db_isref, db_creator, db_version, db_license, db_modtime, db_cksum = rows[0]
         modified_time = os.path.getmtime(var)
         if db_modtime < modified_time:
-            logging.error("Update of database not done, erase database file and rerun vamtb dbs")
-            logging.error(f"This could also be because you have duplicate var for {varname} (in that case, use vamtb sortvar) ")
+            logging.error("Database contains older data for var {varname}. Not updating. Erase database file (or entries) and rerun vamtb dbs")
+            logging.error(f"This could also be because you have duplicate vars for {varname} (in that case, use vamtb sortvar) ")
             exit(0)
         else:
             logging.debug(f"Var {varname} already in database")
@@ -109,10 +108,12 @@ def store_vars(vars_list, sync = True):
     conn.close()
 
 def get_prop_vars(conn, varname, prop_name):
+
     cur = conn.cursor()
     sql = f"SELECT {prop_name} FROM VARS WHERE VARNAME=?"
     row = (varname,)
     cur.execute(sql, row)
+
     res = cur.fetchall()
     if res:
         return res[0][0]
@@ -124,6 +125,7 @@ def get_prop_files(conn, filename, varname, prop_name):
     sql = f"SELECT {prop_name} FROM FILES WHERE FILENAME=? AND VARNAME=?"
     row = (filename, varname)
     cur.execute(sql, row)
+
     res = cur.fetchall()
     if res:
         return res[0][0]
@@ -149,29 +151,6 @@ def var_exists(conn, varname):
     else:
         return (get_prop_vars(conn, varname, "VARNAME") != None)
 
-def get_depending_var_from_file(conn, filename):
-    var, file = filename.split(':')
-    if var.endswith(".var"):
-        var = var[0:-4]
-    filename = f"{var}:{file}"
-
-    cur = conn.cursor()
-    sql = f"SELECT VAR FROM DEPS WHERE DEP=?"
-    row = (filename,)
-    cur.execute(sql, row)
-    res = cur.fetchall()
-
-    a,b,c = var.split('.')
-    default_var_filename = f"{a}.{b}.latest:{file}"
-    sql = f"SELECT VAR FROM DEPS WHERE DEP=?"
-    row = (default_var_filename,)
-    cur.execute(sql, row)
-    res2 = cur.fetchall()
-
-    res.extend(res2)
-
-    return [ v[0] for v in res ]
-
 def isfile_invar(conn, mfile, var):
     cur = conn.cursor()
     sql="SELECT FILENAME FROM FILES WHERE FILENAME = ? AND VARNAME = ?"
@@ -182,7 +161,6 @@ def isfile_invar(conn, mfile, var):
         return False
 
 def latest(conn, var):
-    #Remove version and extension 
     var = ".".join(var.split('.',2)[0:2])
 
     cur = conn.cursor()
@@ -192,6 +170,7 @@ def latest(conn, var):
 
     versions = [ e[0].split('.',3)[2] for e in cur.fetchall() ]
     versions.sort(key=int, reverse=True)
+
     if versions:
         return f"{var}.{versions[0]}"
     else:
@@ -201,13 +180,12 @@ desc_deps=[]
 
 def deps_desc_node(conn, var):
     global desc_deps
+    uniq = set()
 
     cur = conn.cursor()
     sql="SELECT DEP FROM DEPS WHERE VAR = ? COLLATE NOCASE"
     row = (var, )
     cur.execute(sql, row)
-
-    uniq = set()
 
     for depvar in [ e[0].split(':')[0] for e in cur.fetchall() ]:
         if "latest" in depvar:
@@ -253,10 +231,7 @@ def deps_node(conn, var):
 def set_props(conn, var_list):
     res = []
     for var in var_list:
-        if var_exists(conn, var):
-            res.append(f'"{var}" [color=blue];')
-        else:
-            res.append(f'"{var}" [color=red];')
+        res.append(f'"{var}" [color={"blue" if var_exists(conn, var) else "red"}];')
         license = get_license(conn, var)
         if license in ("PC", "Questionable"):
             res.append(f'"{var}" [shape=box];')
@@ -266,6 +241,7 @@ def dotty(lvar=None):
 
     direct_graphs=[]
     shapes = []
+    cmddot = "c:\\Graphviz\\bin\\dot.exe"
     
     conn = sqlite3.connect('vars.db')
     if lvar:
@@ -300,13 +276,13 @@ def dotty(lvar=None):
 
         pdfname = f"VAM_{lvar}.pdf" if lvar else "VAM_deps.pdf"
         try:
-            subprocess.check_call(f'c:\\Graphviz\\bin\\dot.exe -Gcharset=latin1 -Tpdf -o "{pdfname}" deps.dot')
+            subprocess.check_call(f'{cmddot} -Gcharset=latin1 -Tpdf -o "{pdfname}" deps.dot')
         except Exception as CalledProcessError:
-            logging.error("You need dot from graphviz installed in c:\\Graphviz\\bin\\dot.exe")
+            logging.error("You need graphviz installed and dot available in {cmddot}")
             os.unlink("deps.dot")
             exit(0)
         os.unlink("deps.dot")
         logging.info("Graph generated")
 
     else:
-        logging.warning("No graph as no var linked to it")
+        logging.warning("No graph as no var linked to {lvar}")
